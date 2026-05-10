1262b064dd89417425ecdeb1aeda64ef9df5a28c
MMTorre <mmtorre19@gmail.com>
2026-05-10 19:21:23 +0200

    Add enviroment variable frontend to conect backend

---
+++ backend/src/main/java/com/nutriweb100/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.nutriweb100.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                            "http://localhost:4321", // Puerto por defecto de Astro
+                            "https://tfgnutriwebmmt-production.up.railway.app" //  URL de Railway
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
